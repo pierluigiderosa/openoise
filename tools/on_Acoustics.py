d5f8bdb5857746bd599fa0f061a7217825fc68b8
pierluigi de rosa <pierluigi.derosa@gmail.com>
2021-09-08 16:49:28 +0200

    correction in attenuation

--- tools/on_Acoustics.py
+++ tools/on_Acoustics.py
@@ -684,13 +684,18 @@ class Diffraction3D(object):
         for band in self.level_input:
             wave_length = self.sound_speed / float(band)
             c2nd = (1. + (5 * wave_length / self.epsilon) ** 2) / (1 / 3. + (5 * wave_length / self.epsilon) ** 2)
-            if 40./band * c2nd * wave_length >= -2:
-                Att = 10*Ch*log10(3+(40/wave_length)*self.delta)
+            if 40./wave_length * c2nd * self.epsilon >= -2:
+                Att = 10*Ch*log10(3+(40/wave_length * c2nd * self.epsilon))
                 Att_dic[band] = round(Att, 2)
             else:
                 Att = 0
                 Att_dic[band] = round(Att, 2)
 
+        if Att <0:
+         Att =0
+        if Att >25:
+         Att=25
+         
         return Att_dic
 
     def level3D(self):
